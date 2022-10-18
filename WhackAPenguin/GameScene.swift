053d22da5b0024add6ac1736946c02642eb8cae2
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2022-10-18 23:49:41 +0600

    project setup

---
+++ WhackAPenguin/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  WhackAPenguin
+//
+//  Created by Shah Md Imran Hossain on 18/10/22.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+            
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
